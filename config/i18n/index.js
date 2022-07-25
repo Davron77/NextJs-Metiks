48e252a862e3d84d2bfe322beb7b28849dcc1e0d
Davron77 <davronbekq467@gmail.com>
2022-07-25 16:56:31 +0500

    some fixed

--- config/i18n/index.js
+++ config/i18n/index.js
@@ -1,6 +1,6 @@
 // ** I18n Imports
 import i18n from 'i18next'
-import Backend from 'i18next-xhr-backend'
+import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
